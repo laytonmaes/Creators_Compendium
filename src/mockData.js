71994d99e94f7bd806e243a08dcff69b7bd059d2
Dylan Duke <laytonmaes@gmail.com>
2022-09-13 16:29:44 -0600

    add mock data for basic notes

---
+++ src/mockData.js
@@ -0,0 +1,39 @@
+const data = {
+    defaultTags: [
+        {name:"Event", colorHex: "6d5353"},
+        {name:"Character", colorHex: "465B3C"},
+        {name:"Location", colorHex: "465B3C"},
+        {name:"Faction", colorHex: "465B3C"},
+        {name:"Scribble", colorHex: "465B3C"}
+    ],
+    userTags:[],
+    basicNotes:[
+        {
+            name: "The Happening",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris....",
+            tag: {name:"Event", colorHex: "6d5353"}
+        },
+        {
+            name: "Maybe janice Kills a Sad Boy?",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris....",
+            tag: {name:"Scribble", colorHex: "465B3C"}
+        },
+        {
+            name: "Janice Knife Hands",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris....",
+            tag: {name:"Character", colorHex: "465B3C"}
+        },
+        {
+            name: "Psychic Sad Boys",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris....",
+            tag: {name:"Faction", colorHex: "465B3C"}
+        },
+        {
+            name: "The undoing",
+            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris....",
+            tag: {name:"Event", colorHex: "6d5353"}
+        }
+    ]
+}
+
+export default data;
