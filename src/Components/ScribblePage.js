0d37f3c04e1165125fa275d00f5fddd0e837cbef
Dylan Duke <laytonmaes@gmail.com>
2023-04-21 21:13:54 -0600

    Add styling to tags navigation on scribble notes

--- src/Components/ScribblePage.js
+++ src/Components/ScribblePage.js
@@ -11,17 +11,24 @@ const ScribblePage = () => {
     const [notesData, setNotesData] = useState(data.basicNotes)
     return (
         <main>
-            <section className="section-tags">
-                <div className="tags-button">
-                    <img className="banner" src={banner} alt="Tags Banner" />
-                    <h3>Manage Tags</h3>
+            <section className="tags-nav">
+                <div className="tags-div-one">
+                    <button className="tags-button">
+                        <img className="banner" src={banner} alt="Tags Banner" />
+                        <h3>Manage Tags</h3>
+                    </button>
+                    <select className="filter-tags"></select>
+                </div>
+                <div className="tags-div-two">
+                    <input className="search-tags"></input>
                 </div>
                 <span className="splitter-default" />
-                <input className="filter-tags"></input>
             </section>
-            <input className="search-tags"></input>
             <section className="notes">
-                <button className="add-note">+</button>
+                <button className="add-note">
+                    + 
+                    <span className="add-note-name"> Add Note</span>
+                </button>
 
                 <ScribbleNotes notesData={data.basicNotes}/>
             </section>
